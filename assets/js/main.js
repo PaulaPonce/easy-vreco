ef573a71061bc0edb036be55623411d1d1e5461f
Paula Ponce <paula.poncez@usach.cl>
2017-06-19 18:35:12 -0400

    ruta

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,38 +7,66 @@ function initMap(){
 		streetViewControl: false
 	});
 
-	function buscar(){
+	function geoFind(){
 		if(navigator.geolocation){
-			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+			navigator.geolocation.getCurrentPosition(geo_succes, geo_error, geo_options);
 		}
 	}
 
-	document.getElementById("encuentrame").addEventListener("click", buscar);
+	document.getElementById("encuentrame").addEventListener("click", geoFind);
+	var latitude, longitude;
 
-	var latitud, longitud;
-
-	var funcionExito = function(posicion){
-		latitud = posicion.coords.latitude;
-		longitud = posicion.coords.longitude;
+	var geo_succes = function(position){
+		latitude = position.coords.latitude;
+		longitude = position.coords.longitude;
 
 		var miUbicacion = new google.maps.Marker({
-			position: {lat:latitud, lng:longitud},
+			position: {lat:latitude, lng:longitude},
 			animation: google.maps.Animation.DROP,
 			map: map
 		});
 
 		map.setZoom(17);
-		map.setCenter({lat:latitud, lng:longitud});
+		map.setCenter({lat:latitude, lng:longitude});
 	}
 
-	var funcionError = function(error){
+	var geo_error = function(error){
 		alert("Tenemos un problema con encontrar tu ubicación");
 	}
 
+	var geo_options = {
+	enableHighAccuracy: true, 
+	maximumAge: 30000, 
+	timeout: 27000
+	};
+
 	//Autocompletado Input
 	var autocompleteOrigen = new google.maps.places.Autocomplete(document.getElementById("origen"));
-	autocompleteOrigen.bindTo('bounds', map);
+	autocompleteOrigen.bindTo("bounds", map);
 	var autocompleteDestino = new google.maps.places.Autocomplete(document.getElementById("destino"));
-	autocompleteDestino.bindTo('bounds', map);
+	autocompleteDestino.bindTo("bounds", map);
+
+	//Ruta
+	document.getElementById("ruta").addEventListener("click", calcRoute);
+
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+	var directionsService = new google.maps.DirectionsService();
+
+	function calcRoute(){
+		var start = document.getElementById("origen").value;
+		var end = document.getElementById("destino").value;
+		var request = {
+			origin: start,
+			destination: end,
+			travelMode: "DRIVING"
+		};
+		directionsService.route(request, function(result, status){
+			if(status == "OK"){
+				directionsDisplay.setDirections(result);
+			}
+		});
+	}
+	directionsDisplay.setMap(map);
+	
 }
 initMap();
